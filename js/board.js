0056eddc0b32230560eaf2a9aa26dfb098427d6b
TwoThousandDollars <bbrooks1@harding.edu>
2021-11-26 12:28:45 -0600

    completed region highlight feature

--- js/board.js
+++ js/board.js
@@ -23,22 +23,14 @@ export default class Board {
         this.highlightColumn(e);
         this.highlightRow(e);
         this.highlightRegion(e);
-        e.target.classList.add('highlighted');
     }
 
-    highlightIndividualCell(cell) {
-        cell.classList.add('highlighted')
-    }
-
+    
     highlightColumn(e) {
-        // find the index of the element that has been clicked on
-        // loop through each row and highlight the element of the same index
         let column = this.getColumn(e);
-        column.forEach(cell => {
-            this.highlightIndividualCell(cell);
-        })
+        this.highlightAllCells(column);
     }
-
+    
     getColumn(e) {
         let columnNumber = this.getRow(e).indexOf(e.target)
         let column = [];
@@ -46,37 +38,50 @@ export default class Board {
             column.push(row[columnNumber]);
         });
         return column;
-        // find the row where the cell is in
-        // find the index of the cell within that row
-
     }
-
+    
     highlightRow(e) {
         let row = this.getRow(e);
-        row.forEach(cell => {
-            this.highlightIndividualCell(cell);
-        })
+        this.highlightAllCells(row);
     }
-
+    
     getRow(e) {
         let row = Array.from(e.target.parentNode.childNodes)
         row = row.filter(this.filterOutTextNodes);
         return row;
     }
-
+    
     filterOutTextNodes(value) {
         return value.nodeType != 3;
     }
-
+    
     highlightRegion(e) {
-        // find the region the element belongs to 
-        // fill each of the cells in that region
+        let region = this.getRegion(e);
+        this.highlightAllCells(region);
     }
-
-    cellArrayFill(arrayOfCells) {
-        // receives an array of cells and highlights all of them 
+    
+    getRegion(e) {
+        let regionNumber = this.getRegionNumber(e);
+        let region = document.getElementsByClassName(regionNumber);
+        return Array.from(region);
     }
-
+    
+    getRegionNumber(e) {
+        let cellClasses = Array.from(e.target.classList);
+        let re = /region\-\d/;
+        let regionNumber = cellClasses.find(value => re.test(value));
+        return regionNumber;
+    }
+    
+    highlightAllCells(arrayOfCells) {
+        arrayOfCells.forEach(cell => {
+            this.highlightIndividualCell(cell);
+        })
+    }
+    
+    highlightIndividualCell(cell) {
+        cell.classList.add('highlighted')
+    }
     clearHighlightedCells() {
         this.cells.forEach(cell => { cell.classList.remove('highlighted')})
     }
