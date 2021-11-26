2c2a4beb22a72a84f9f46208fa278328095080b9
TwoThousandDollars <bbrooks1@harding.edu>
2021-11-26 11:09:09 -0600

    completed row highlight feature

--- js/board.js
+++ js/board.js
@@ -6,28 +6,54 @@ export default class Board {
         
         // add event listeners
         this.cells.forEach(cell => {
-            cell.addEventListener('click', this.highlightCells.bind(this))
+            cell.addEventListener('click', this.highlightRelaventCells.bind(this))
         })
     }
     
-    highlightCells(e) {
+    highlightRelaventCells(e) {
         this.clearHighlightedCells();
-        this.highlightColumn();
-        this.highlightRow();
-        this.highlightArea();
+        this.highlightColumn(e);
+        this.highlightRow(e);
+        this.highlightRegion(e);
         e.target.classList.add('highlighted');
     }
 
-    highlightColumn() {
-
+    highlightIndividualCell(cell) {
+        console.log(cell)
+        cell.classList.add('highlighted')
     }
 
-    highlightRow() {
-
+    highlightColumn(e) {
+        // find the index of the element that has been clicked on
+        // loop through each row and highlight the element of the same index
     }
 
-    highlightArea() {
-        
+    highlightRow(e) {
+        let row = this.getRow(e);
+        row.forEach(cell => {
+            this.highlightIndividualCell(cell);
+        })
+        // find the index of the row that the cell belongs in 
+        // highlight the entire row 
+    }
+
+    getRow(e) {
+        let row = Array.from(e.target.parentNode.childNodes)
+        row = row.filter(this.filterOutTextNodes);
+        return row;
+    }
+
+    filterOutTextNodes(value) {
+        return value.nodeType != 3;
+    }
+
+    highlightRegion(e) {
+        // find the region the element belongs to 
+        // fill each of the cells in that region
+    }
+
+    cellArrayFill(arrayOfCells) {
+        // receives an array of cells and highlights all of them 
     }
 
     clearHighlightedCells() {
