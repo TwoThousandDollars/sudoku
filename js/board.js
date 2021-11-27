d9b5f94f6c5b1896753abe626ea3a287de946234
TwoThousandDollars <bbrooks1@harding.edu>
2021-11-26 18:49:03 -0600

    cache all rows columns and regions

--- js/board.js
+++ js/board.js
@@ -4,23 +4,25 @@ export default class Board {
         this.gameBoard = document.querySelector("#board"),
         this.cells = document.querySelectorAll(".cell"),
         this.rows = this.extractRowsFromDocument(),
-        this.selectedCell = this.cells[0];
+        this.columns = this.extractColumnsFromDocument(),
+        this.regions = this.extractRegionsFromDocument(),
+        this.selectedCell = this.cells[0],
         this.numPad = document.querySelectorAll('.number'),
         this.modeButton = document.querySelector('#modeButton'),
-        this.pencilMarkElement = document.createElement('div');
-        this.pencilMarkElement.classList.add('pencil-mark--container');
-        this.writeMode = 'digit';
+        this.pencilMarkElement = document.createElement('div'),
+        this.pencilMarkElement.classList.add('pencil-mark--container'),
+        this.writeMode = 'digit',
         
         // add event listeners
         this.cells.forEach(cell => {
             cell.addEventListener('click', this.updateRelevantCells.bind(this))
-        });
+        }),
         
         this.numPad.forEach(button => {
-            button.addEventListener('click', this.insertDigit.bind(this));
-        });
+            button.addEventListener('click', this.insertDigit.bind(this))
+        }),
 
-        this.modeButton.addEventListener('click', this.toggleMode.bind(this));
+        this.modeButton.addEventListener('click', this.toggleMode.bind(this)),
         
         // initail board setup 
         this.selectedCell.click();
@@ -29,6 +31,24 @@ export default class Board {
     toggleMode(e) {
         e.target.innerText = (this.writeMode === 'digit') ? 'Notes: ON' : 'Notes: OFF';
         this.writeMode = (this.writeMode === 'digit') ? 'pencilMark' : 'digit';
+    }
+
+    extractRegionsFromDocument() {
+        let regionCells = this.getElementFromEachRegion();
+        let regions = [];
+        regionCells.forEach(cell => {
+            regions.push(this.getRegion(cell));
+        });
+        return regions;
+    }
+
+    getElementFromEachRegion() {
+        let regionArray = [];
+        for (let i=1;i<=9;i++) {
+            let regionNumber = 'region-'.concat(i.toString());
+            regionArray.push(document.getElementsByClassName(regionNumber)[0])
+        }
+        return regionArray;
     }
 
     extractRowsFromDocument() {
@@ -40,6 +60,14 @@ export default class Board {
             rows.push(cleanRow);
         });
         return rows;
+    }
+
+    extractColumnsFromDocument() {
+        let columns = [];
+        this.rows[0].forEach(cell => {
+            columns.push(this.getColumn(cell));
+        });
+        return columns;
     }
     
     updateRelevantCells(e) {
@@ -63,20 +91,20 @@ export default class Board {
     }
 
     highlightAllRelevantCells(e) {
+        let element = e.target;
         this.clearClassFromCells('highlighted');
-        this.highlightColumn(e);
-        this.highlightRow(e);
-        this.highlightRegion(e);
+        this.highlightColumn(element);
+        this.highlightRow(element);
+        this.highlightRegion(element);
     }
 
-    
-    highlightColumn(e) {
-        let column = this.getColumn(e);
+    highlightColumn(element) {
+        let column = this.getColumn(element);
         this.highlightAllCells(column);
     }
     
-    getColumn(e) {
-        let columnNumber = this.getRow(e).indexOf(e.target)
+    getColumn(element) {
+        let columnNumber = this.getRow(element).indexOf(element)
         let column = [];
         this.rows.forEach(row => {
             column.push(row[columnNumber]);
@@ -84,13 +112,13 @@ export default class Board {
         return column;
     }
     
-    highlightRow(e) {
-        let row = this.getRow(e);
+    highlightRow(element) {
+        let row = this.getRow(element);
         this.highlightAllCells(row);
     }
     
-    getRow(e) {
-        let row = Array.from(e.target.parentNode.childNodes)
+    getRow(element) {
+        let row = Array.from(element.parentNode.childNodes)
         row = row.filter(this.filterOutTextNodes);
         return row;
     }
@@ -99,19 +127,19 @@ export default class Board {
         return value.nodeType != 3;
     }
     
-    highlightRegion(e) {
-        let region = this.getRegion(e);
+    highlightRegion(element) {
+        let region = this.getRegion(element);
         this.highlightAllCells(region);
     }
     
-    getRegion(e) {
-        let regionNumber = this.getRegionNumber(e);
+    getRegion(element) {
+        let regionNumber = this.getRegionNumber(element);
         let region = document.getElementsByClassName(regionNumber);
         return Array.from(region);
     }
     
-    getRegionNumber(e) {
-        let cellClasses = Array.from(e.target.classList);
+    getRegionNumber(element) {
+        let cellClasses = Array.from(element.classList);
         let re = /region\-\d/;
         let regionNumber = cellClasses.find(value => re.test(value));
         return regionNumber;
